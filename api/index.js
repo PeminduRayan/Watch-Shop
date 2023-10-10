41ad522a5786b6bdd4e0405ee555a956d3156a15
PeminduRayan <pemirayan@gmail.com>
2023-10-11 00:10:59 +0530

    Connected to mongoDB

--- api/index.js
+++ api/index.js
@@ -1,6 +1,19 @@
+import dotenv  from 'dotenv';
 import express from 'express';
+import mongoose from 'mongoose'
+dotenv.config();
+
 const app = express();
 const port = 4000;
+mongoose
+.connect(process.env.MONGO)
+.then(()=>{
+    console.log("Connected to DB");
+
+})
+.catch((err) => {
+    console.log(err)
+})
 
 app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Example app listening on port ${port}! `));
