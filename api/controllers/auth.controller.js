52a8be5d853ee23c7cc19dd9e4bf87b471dad605
PeminduRayan <pemirayan@gmail.com>
2023-10-11 19:34:21 +0530

    Middleware for error handling was added

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,16 +1,15 @@
 import User from "../models/user.model.js";
-import bcrtptjs from 'bcryptjs';
+import bcrtptjs from "bcryptjs";
+import {errorHandler} from '../utils/error.js'
+export const signup = async (req, res, next) => {
+  const { username, email, password } = req.body;
+  const hashedPassword = bcrtptjs.hashSync(password, 10);
 
-export const signup = async (req,res) => {
-    const {username, email, password} = req.body;
-    const hashedPassword = bcrtptjs.hashSync(password,10);
-    
-    const newUser = new User({username,email,password: hashedPassword});
-    try {
-     
+  const newUser = new User({ username, email, password: hashedPassword });
+  try {
     await newUser.save();
-    res.status(201).json("User created Successfully!");   
-    } catch (error) {
-         res.status(500).json(error.message);
-    }
-}
+    res.status(201).json("User created Successfully!");
+  } catch (error) {
+    next(errorHandler(550,'Error in the function'));
+  }
+};
